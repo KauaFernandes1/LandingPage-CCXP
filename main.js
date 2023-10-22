6c5475f47046ec46d867ae7d4f7bf6956de8792c
Kauã Fernandes <113432945+KauaFernandes1@users.noreply.github.com>
2023-10-22 12:10:36 -0300

    Update Main.js, Interação

--- main.js
+++ main.js
@@ -15,3 +15,51 @@ function highlightCard(selector)
     var element = document.querySelector(selector);
     element.classList.toggle("card-highlight");
 }
+
+function checkKeyboardCode()
+{
+    document.addEventListener('keydown', (event) => {
+        var name = event.key;
+        var code = event.code;
+    }, false)
+}
+
+document.addEventListener(`keydown`, (event) => {
+
+    var ingresso1 = document.getElementById("quinta");
+    var ingresso2 = document.getElementById("sexta");
+    var ingresso3 = document.getElementById("sabado");
+    var ingresso4 = document.getElementById("domingo");
+   
+    var code = event.code;
+    if(code == 'Digit1') {
+        ingresso1.classList.toggle("card-highLight")
+        ingresso2.classList.remove("card-highLight")
+        ingresso3.classList.remove("card-highLight")
+        ingresso4.classList.remove("card-highLight")
+
+    }
+
+    if(coce == "Digit2"){
+        ingresso1.classList.remove("card-highLight")
+        ingresso2.classList.toggle("card-highLight")
+        ingresso3.classList.remove("card-highLight")
+        ingresso4.classList.remove("card-highLight")
+    }
+    if(coce == "Digit3"){
+        ingresso1.classList.remove("card-highLight")
+        ingresso2.classList.remove("card-highLight")
+        ingresso3.classList.toggle("card-highLight")
+        ingresso4.classList.remove("card-highLight")
+    }
+    if(coce == "Digit4"){
+        ingresso1.classList.remove("card-highLight")
+        ingresso2.classList.remove("card-highLight")
+        ingresso3.classList.remove("card-highLight")
+        ingresso4.classList.toggle("card-highLight")
+    }
+}, false);
+
+
+checkKeyboardCode();
+
